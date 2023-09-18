8e3fce3b873fe77031845be1dc60148d045c3617
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2023-09-18 19:08:11 +0100

    Organise files (#3)
    Organise files, del unused code, prod newest image

--- spec/helpers/renderer.js
+++ spec/helpers/renderer.js
@@ -1,41 +1,18 @@
 import { JSDOM } from 'jsdom';
 
-const domJSDOM = new JSDOM(undefined, {
+const globalJSDOM = new JSDOM(undefined, {
     runScripts: "dangerously",
     pretendToBeVisual: true,
     resources: "usable",
     includeNodeLocations: true
 });
 
-const windowJSDOM = domJSDOM.window
+const windowJSDOM = globalJSDOM.window
 
 Object.defineProperty(globalThis, "window", {
     value: windowJSDOM,
 });
 
-//Object.defineProperty(globalThis.window, "navigator", {
-//    value: windowJSDOM.navigator,
-//});
-
-//Object.defineProperty(globalThis.window.navigator, "clipboard", {
-//    value: {},
-//});
-
-function render(html) {
-	const container = windowJSDOM.document.createElement('div');
-	container.innerHTML = html;
-	const queryByTestId = testId => {
-		return container.querySelector(`[data-testid="${testId}"]`);
-	};
-	windowJSDOM.document.body.appendChild(container);
-	return {
-		container,
-		queryByTestId,
-	};
-}
-
 export {
-    domJSDOM,
-    windowJSDOM,
-    render
+    globalJSDOM
 }
